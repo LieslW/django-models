b12ab8d704e95829a08a99da3ccc99b693433777
Liesl White <lieslfwhite@gmail.com>
2022-09-19 05:34:58 -0700

    model initialized

--- snack_tracker_project/snacks/urls.py
+++ snack_tracker_project/snacks/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
-from .views import HomePageView, AboutView
-
+from django.urls import include, path
+from .views import SnackDetailView, SnackListView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
-    path("about/", AboutView.as_view(), name="about"),
+    path('', SnackListView.as_view(), name="snack_list"),
+    path('<int:pk>/', SnackDetailView.as_view(), name="snack_detail"),
 ]
--- snack_tracker_project/snacks/views.py
+++ snack_tracker_project/snacks/views.py
@@ -1,9 +1,13 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Snack
 
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+class SnackListView(ListView):
+    template_name = "snack_list.html"
+    model = Snack
 
 
-class AboutView(TemplateView):
-    template_name = 'about.html'
+class SnackDetailView(DetailView):
+    template_name = "snack_detail.html"
+    model = Snack
